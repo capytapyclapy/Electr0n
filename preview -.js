b2934f5823964146f1347ec0ce36880b419593f3
Nexus <125509370+capytapyclapy@users.noreply.github.com>
2024-09-30 17:05:28 -0500

    Add files via upload

---
+++ preview -.js
@@ -0,0 +1,14 @@
+function launchGame() {
+  const gameWindow = window.open('about:blank', '_blank');
+  gameWindow.document.write('<iframe src="game.html" frameborder="0" width="100%" height="100%"></iframe>');
+}
+// script.js
+const gameButtons = document.querySelectorAll('.game-button');
+
+gameButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const gameName = event.target.dataset.game;
+    const gameWindow = window.open('about:blank', '_blank');
+    gameWindow.document.write(`<iframe src="${gameName}.html" frameborder="0" width="100%" height="100%"></iframe>`);
+  });
+});
