33580d1ade3f843f67415672e9159e9ca6b4d3ce
Nexus <125509370+capytapyclapy@users.noreply.github.com>
2024-09-30 18:29:37 -0500

    preview -.js

--- preview -.js
+++ preview -.js
@@ -2,6 +2,7 @@ function launchGame() {
   const gameWindow = window.open('about:blank', '_blank');
   gameWindow.document.write('<iframe src="game.html" frameborder="0" width="100%" height="100%"></iframe>');
 }
+
 // script.js
 const gameButtons = document.querySelectorAll('.game-button');
 
